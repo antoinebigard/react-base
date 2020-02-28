c6ea8afd7430d0d5dae0278955b0798a3b2b8346
Gethi <bigard.antoine@gmail.com>
2020-02-28 11:59:35 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
+import WebFont from "webfontloader";
 
 import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
 import { Landing, NotFound, SignIn, Profile, ForceChangePassword } from "./pages";
 
 import routes from "./routes.json";
 
 import "./App.css";
+WebFont.load({ google: { families: ["Josefin Sans: 200, 300, 400, 600, 700", "Raleway"] } });
 
 const App = () => (
   <Layout>
+    <ScrollToTop />
     <Switch>
       <Route exact path="/" component={Landing} />
       <Route exact path={routes.SIGNIN} component={SignIn} />
